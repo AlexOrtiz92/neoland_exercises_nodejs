b5129c7f535634dfb0203975325efdbe58e5dcb9
AlexOrtiz92 <alejandro.ortiz.ortega@gmail.com>
2021-01-30 09:33:57 +0100

    feat(PokemonAPI)

---
+++ PokeAPI/db/dbPokemon.js
@@ -0,0 +1,23 @@
+//Esto es nuestra base de datos falsa.
+const pokemon = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    type: "Planta"
+  },
+  {
+    id: 4,
+    name: "Squirtle",
+    type: "Agua"
+  },
+  {
+    id: 7,
+    name: "Charmander",
+    type: "Fuego"
+  }
+
+]
+
+module.exports = {
+  pokemon: pokemon
+}
